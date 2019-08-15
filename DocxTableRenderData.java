a2ad951b4f21fc6b3bec76cbac474657d821972a
wujianbin-sh <wujianbin.sh@qq.com>
2019-08-15 16:06:45 +0800

    Create DocxTableRenderData.java

---
+++ DocxTableRenderData.java
@@ -0,0 +1,31 @@
+package com.poitlplus;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.deepoove.poi.data.DocxRenderData;
+
+import lombok.Getter;
+
+/**
+ * 
+ * @author WuJianbin
+ */
+public class DocxTableRenderData extends DocxRenderData {
+	@Getter
+	Function<Object, Map<String, Object>> functionToGetTableData;
+
+	public DocxTableRenderData(String fileName, List<?> dataListForPages,
+			Function<Object, Map<String, Object>> functionToGetTableData) {
+		super(new File(fileName), dataListForPages);
+		this.functionToGetTableData = functionToGetTableData;
+	}
+
+	public DocxTableRenderData(File docxFile, List<?> dataListForPages,
+			Function<Object, Map<String, Object>> functionToGetTableData) {
+		super(docxFile, dataListForPages);
+		this.functionToGetTableData = functionToGetTableData;
+	}
+}
